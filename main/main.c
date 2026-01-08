a904414974bfd955d2c357a36822ace9cb7dc252
dexse0t1QZF70xwy* <becario@3code.us>
2026-01-08 14:28:08 -0600

    gitignore change

--- main/main.c
+++ main/main.c
@@ -1,20 +1,8 @@
-#include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
 #include "leds_pwr_manager.h"
 #include "uart_manager.h"
 #include "nvsManager.h"
 #include "deviceManager.h"
 #include "eventHandler.h"
-
-
-#include "esp_log.h"
-#include "esp_check.h"
 
 void app_main(void)
 {
